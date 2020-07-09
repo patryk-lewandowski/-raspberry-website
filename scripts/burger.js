2cf6d416171fb062fb1b0c412f276d824dea670d
Patryk Lewandowski <patryklewandowski17@gmail.com>
2020-07-09 19:26:04 +0200

    speed up menu animation

--- scripts/burger.js
+++ scripts/burger.js
@@ -5,20 +5,20 @@ const speciesHeader = document.querySelector('.species__title');
 burgerIcon.addEventListener('click', () => {
     burgerIcon.classList.toggle('head__burger-icon--active');
     if (burgerIcon.classList.contains('icon-menu')) {
-        burgerIcon.style.animation = 'menu-out 0.5s linear both';
+        burgerIcon.style.animation = 'menu-out 0.2s linear both';
         setTimeout(function () {
             menu.classList.toggle('menu--active');
             burgerIcon.classList.toggle('icon-cancel');
             burgerIcon.classList.toggle('icon-menu');
-            burgerIcon.style.animation = 'cancel-in 0.5s linear both';
+            burgerIcon.style.animation = 'cancel-in 0.2s linear both';
         }, 500);
     } else if (burgerIcon.classList.contains('icon-cancel')) {
-        burgerIcon.style.animation = 'cancel-out 0.5s linear both';
+        menu.classList.toggle('menu--active');
+        burgerIcon.style.animation = 'cancel-out 0.2s linear both';
         setTimeout(function () {
-            menu.classList.toggle('menu--active');
             burgerIcon.classList.toggle('icon-cancel');
             burgerIcon.classList.toggle('icon-menu');
-            burgerIcon.style.animation = 'menu-in 0.5s linear both';
+            burgerIcon.style.animation = 'menu-in 0.2s linear both';
         }, 500);
     }
 
@@ -26,7 +26,7 @@ burgerIcon.addEventListener('click', () => {
 })
 
 const changeSpeciesHeader = () => {
-    if (window.innerWidth > 1281) {
+    if (window.innerWidth > 1200) {
         speciesHeader.innerHTML = 'The Best Raspberry Varieties';
     } else {
         speciesHeader.innerHTML = 'The Best </br> Raspberry Varieties';
